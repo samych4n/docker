bf70361845a377f284d85808189d69b5bf540e96
samychan <lelouchds@gmail.com>
2020-08-16 14:23:57 -0300

    laravel ok

--- html/blog/routes/api.php
+++ html/blog/routes/api.php
@@ -13,7 +13,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::get('users/{id}', function ($id) {
+    return ['url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),];
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
